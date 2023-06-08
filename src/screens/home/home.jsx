f475d495f06ba721b9fb6bac8a09317815e578e2
Ngô Vưn Nam <anh02284@gmail.com>
2023-06-09 02:23:14 +0700

    feat: book new screen home

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,11 +1,36 @@
-import { memo } from 'react';
-import { View } from 'react-native';
-import { Banner } from '../../components';
+import { memo, useMemo } from "react";
+import { View } from "react-native";
+import _ from "lodash";
+import { Banner, CategoryBook, tw } from "../../components";
+import { useQueryBooks } from "../../hooks";
+import dayjs from "dayjs";
+import { Text } from "@rneui/themed";
 
 export const Home = memo(({ navigation }) => {
+  const { data, isFetching, isLoading, isError, error } = useQueryBooks();
+  const bookNew = useMemo(() => {
+    let results = [];
+    data &&
+      _.sortBy(data)
+        .reverse()
+        .forEach((item, idx) => {
+          if (idx >= 14) return;
+          results = [...results, item];
+        });
+    return results;
+  }, [data]);
   return (
     <View>
       <Banner />
+      {isLoading || isFetching ? (
+        <View style={tw`flex justify-center items-center`}>
+          <Text>Đang tải...</Text>
+        </View>
+      ) : null}
+      {isError ? <Text>{error.message}</Text> : null}
+      <View style={tw`px-16px`}>
+        <CategoryBook data={bookNew} topic={"Sách mới"} />
+      </View>
     </View>
   );
 });
