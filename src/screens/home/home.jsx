569e64606dec053f968f8c93c216a9c9445d4a4b
Ngô Vưn Nam <anh02284@gmail.com>
2023-06-10 13:05:25 +0700

    fix: warning

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,12 +1,14 @@
-import { memo, useMemo } from 'react';
-import { ScrollView, View } from 'react-native';
-import _ from 'lodash';
-import { Banner, GridBook, tw } from '../../components';
-import { useQueryBooks } from '../../hooks';
-import { Text } from '@rneui/themed';
+import { memo, useMemo } from "react";
+import { ScrollView, View } from "react-native";
+import _ from "lodash";
+import { Banner, GridBook, tw } from "../../components";
+import { useQueryBooks } from "../../hooks";
+import { Text } from "@rneui/themed";
+import { Screen } from "react-native-screens";
 
 export const Home = memo(() => {
-  const { data, isFetching, isLoading, isError, status, error } = useQueryBooks();
+  const { data, isFetching, isLoading, isError, status, error } =
+    useQueryBooks();
   const bookNew = useMemo(() => {
     let results = [];
     data &&
@@ -28,22 +30,34 @@ export const Home = memo(() => {
   }, [data]);
 
   return (
-    <ScrollView style={tw`bg-white`}>
-      <Banner />
-      {isLoading || isFetching ? (
-        <View style={tw`flex justify-center items-center`}>
-          <Text>Đang tải...</Text>
+    <Screen style={tw`bg-white`}>
+      <ScrollView>
+        <Banner />
+        {isLoading || isFetching ? (
+          <View style={tw`flex justify-center items-center`}>
+            <Text>Đang tải...</Text>
+          </View>
+        ) : null}
+        {isError ? <Text>{error.message}</Text> : null}
+        <View>
+          <Text style={tw`px-12px pt-16px font-bold text-18px`}>Sách mới</Text>
+          <GridBook
+            status={status}
+            data={bookNew}
+            error={error}
+            isFetching={isFetching}
+          />
         </View>
-      ) : null}
-      {isError ? <Text>{error.message}</Text> : null}
-      <View>
-        <Text style={tw`px-12px pt-16px font-bold text-18px`}>Sách mới</Text>
-        <GridBook status={status} data={bookNew} error={error} isFetching={isFetching} />
-      </View>
-      <View>
-        <Text style={tw`px-12px pt-16px font-bold text-18px`}>Sách phổ biến</Text>
-        <GridBook status={status} data={booksPopular} error={error} isFetching={isFetching} />
-      </View>
-    </ScrollView>
+        <Text style={tw`px-12px pt-16px font-bold text-18px`}>
+          Sách phổ biến
+        </Text>
+        <GridBook
+          status={status}
+          data={booksPopular}
+          error={error}
+          isFetching={isFetching}
+        />
+      </ScrollView>
+    </Screen>
   );
 });
