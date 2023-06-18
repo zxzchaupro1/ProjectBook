8f519971753b24cb48ec1a811be95a95c1b36e1f
nvnam042dev <anh02284@gmail.com>
2023-06-18 11:51:24 +0700

    feat: hidden warning

--- App.js
+++ App.js
@@ -9,10 +9,10 @@ import { AppNavigator } from "./src/navigator";
 import { LogBox } from "react-native";
 
 const IGNORED_LOGS = [
-  // "Non-serializable values were found in the navigation state",
-  // "Require cycle:",
-  // "source.uri should not be an empty string",
-  // "You are not currently signed in to Expo on your development machine",
+  "Non-serializable values were found in the navigation state",
+  "Require cycle:",
+  "source.uri should not be an empty string",
+  "You are not currently signed in to Expo on your development machine",
 ];
 
 LogBox.ignoreLogs(IGNORED_LOGS);
